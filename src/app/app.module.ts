5fc2c4f1aca349e1020cc9ff74ab79dd6f2d1da6
kv-2 <str_token@mail.ru>
2019-06-29 18:33:01 +0300

    add component summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { StocksService } from './services/stocks.service';
+import { SummaryComponent } from './components/summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
