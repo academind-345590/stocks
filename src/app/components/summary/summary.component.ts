046a307ef973a02c28f1946f6a093d2a67e7ade7
kv-2 <str_token@mail.ru>
2019-06-29 20:05:50 +0300

    add summary vomponent to controller

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+// imports the dependencies
+import { Component, Input } from '@angular/core';
+//declares the component metadata
 @Component({
-  selector: 'app-summary',
+  selector: 'summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.css']
 })
-export class SummaryComponent implements OnInit {
+//exports the summary component class
+export class SummaryComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  //declared a property that is an input value
+  @Input() stock: any;
+  //method to check if stock is negative
+  isNegative() {
+    return (this.stock && this.stock.change < 0);
+  }
+  //method to check if stock is positive
+  isPositive() {
+    return (this.stock && this.stock.change > 0)
   }
 
 }
