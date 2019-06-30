ba77b7175dd22e211e0b6761480a69b20470eae9
kv-2 <str_token@mail.ru>
2019-06-30 18:44:22 +0300

    update conteoller by manage component

--- src/app/components/manage/manage.component.ts
+++ src/app/components/manage/manage.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+//import dependence
+import { Component } from '@angular/core';
+import { StocksService } from '../../services/stocks.service';
 
+//declares component metadata
 @Component({
-  selector: 'app-manage',
+  selector: 'manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.css']
 })
-export class ManageComponent implements OnInit {
+// defines class and two properties for storing the array of symbols and a string for input
+export class ManageComponent {
+  symbols: Array<string>;
+  stock: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  // get the current list of symbols duting class instantiation
+  constructor(private service: StocksService) {
+    this.symbols = service.get();
   }
 
+  //method to sdd a new stock to the list
+  add() {
+    this.symbols = this.service.add(this.stock.toUpperCase());
+    this.stock = '';
+  }
+
+  // method to remove a stock symbol from the list
+  remove(symbol) {
+    this.symbols = this.service.remove(symbol);
+  }
 }
