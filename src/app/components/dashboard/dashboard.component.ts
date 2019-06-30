a2d5403b14e98b46e817aa056f9cab8420561038
kv-2 <str_token@mail.ru>
2019-06-30 17:04:11 +0300

    revemp controller

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -11,6 +11,8 @@ import { StocksService, StockInterface } from '../../services/stocks.service';
 export class DashboardComponent implements OnInit {
   //declares a property for hlding an array of stocks
   stocks: Array<StockInterface>;
+  //declares a property for holding an array of stock symbols
+  symbols: Array<string>;
   // gets the stocks symbols from the service when the cimponent is first constructed
   constructor(private service: StocksService) {
     this.symbols = service.get();
