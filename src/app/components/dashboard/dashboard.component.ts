e14950670ba3d3fa4ec4339b80737c0165370d9e
kv-2 <str_token@mail.ru>
2019-06-30 16:53:25 +0300

    replace comtroller

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+// import dependencies
+import { StocksService, StockInterface } from '../../services/stocks.service';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+//export the component class, but also implements the OnInit interface
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  //declares a property for hlding an array of stocks
+  stocks: Array<StockInterface>;
+  // gets the stocks symbols from the service when the cimponent is first constructed
+  constructor(private service: StocksService) {
+    this.symbols = service.get();
   }
-
+  //implements the ngOInit methd and calls the service to load stock data over Http
+  ngOnInit() {
+    this.service.load(this.symbols).subscribe(stocks => this.stocks = stocks);
+  }
 }
