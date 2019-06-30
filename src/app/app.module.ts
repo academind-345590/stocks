1bb1342ca0cbd0c8e01fd0a7e42e9fc8b4373cda
kv-2 <str_token@mail.ru>
2019-06-30 18:26:24 +0300

    add the FormsModule to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { StocksService } from './services/stocks.service';
 import { SummaryComponent } from './components/summary/summary.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ManageComponent } from './components/manage/manage.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { ManageComponent } from './components/manage/manage.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
