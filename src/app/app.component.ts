1ff904fb27e0ad5e756977c437340c6ef6d50d7f
kv-2 <str_token@mail.ru>
2019-06-30 16:23:59 +0300

    cleaned up the App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,8 @@
 import { Component } from '@angular/core';
-// import the stockinterface
-import { StocksService, StockInterface } from './services/stocks.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  //declares a proporty of an array of stocks
-  stocks: Array<StockInterface>;
-
-  constructor(service: StocksService) {
-    service.load(['AAPL']).subscribe(stocks => {
-      //when the data loads, it will store it on the stocks property
-      this.stocks = stocks;
-    });
-  }
-
-}
+export class AppComponent { }
